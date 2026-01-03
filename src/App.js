3908d345b062f8d2a5e08415cf86cdee24bfbea6
Jomarie Tijada <jstijada@unp.edu.ph>
2026-01-03 15:58:20 +0800

    Fresh start with Tailwind v3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import Hello from "./components/Hello";
+import './index.css'
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className="text-3xl font-bold underline text-red-500">
+      Hello Tailwind!
+    </h1>
     </div>
   );
 }
